3d8da520b69ed98a2902c13adde334c42e6a74ec
Nicolas Cavilla <nico_cavi@hotmail.com>
2021-01-02 20:37:00 -0300

    formulario

--- src/app/formulario-crear/formulario-crear.component.ts
+++ src/app/formulario-crear/formulario-crear.component.ts
@@ -1,25 +1,39 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-crear',
   templateUrl: './formulario-crear.component.html',
   styleUrls: ['./formulario-crear.component.css']
 })
-export class FormularioCrearComponent {
+export class FormularioCrearComponent implements OnInit{
 
-  model: any = {};
+  formulario:FormGroup;
   msg: string = "";
-
-  empleados = [
-    {name: 'Jorge', position: 'Programador', email:'jorgito10@gmail.com'}
-  ];
+  empleados = [];
 
   @Output() mensaje = new EventEmitter<string>();
   @Output() listado = new EventEmitter<any>();
 
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(){
+    this.formulario = this.formBuilder.group({
+      name: ['', Validators.required],
+      position: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
   addEmployee():void {
-    this.empleados.push(this.model);
-    this.model = {};
+
+    const object = {
+      name: this.formulario.value['name'],
+      position: this.formulario.value['position'],
+      email: this.formulario.value['email'],
+    }
+
+    this.empleados.push(object);
     this.msg = "Empleado Agregado";
     this.listado.emit(this.empleados);
 
